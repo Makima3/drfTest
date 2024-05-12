f2851d9c8b67981c45ecb4614fd911f68e014c22
Makima3 <natalia.kolchuk@gmail.com>
2024-05-13 00:24:15 +0300

    lesson 5 pagination

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework.pagination import PageNumberPagination
+# у view ми можемо передавати пагінований список
 
 from .serializers import CarModelSerializer
 from .models import CarModel
@@ -7,6 +9,8 @@ from .models import CarModel
 class CarListView(ListAPIView):
     queryset = CarModel.objects.all()
     serializer_class = CarModelSerializer
+    # pagination_class = PageNumberPagination # імпортуємо та підключаємо пагінацію
+    permission_classes =
 
 
 class CarRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
